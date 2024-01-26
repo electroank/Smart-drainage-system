9d1e1de90b5448609f375634bd743428d97f3839
electroank <ankan4135@gmail.com>
2024-01-26 10:54:17 +0530

    Update Edgent_ESP32_oled_bmp180.ino

--- Smart_drainage_system/Edgent_ESP32_oled_bmp180.ino
+++ Smart_drainage_system/Edgent_ESP32_oled_bmp180.ino
@@ -1,31 +1,7 @@
-/*************************************************************
-  Blynk is a platform with iOS and Android apps to control
-  ESP32, Arduino, Raspberry Pi and the likes over the Internet.
-  You can easily build mobile and web interfaces for any
-  projects by simply dragging and dropping widgets.
 
-    Downloads, docs, tutorials: https://www.blynk.io
-    Sketch generator:           https://examples.blynk.cc
-    Blynk community:            https://community.blynk.cc
-    Follow us:                  https://www.fb.com/blynkapp
-                                https://twitter.com/blynk_app
-
-  Blynk library is licensed under MIT license
- *************************************************************
-  Blynk.Edgent implements:
-  - Blynk.Inject - Dynamic WiFi credentials provisioning
-  - Blynk.Air    - Over The Air firmware updates
-  - Device state indication using a physical LED
-  - Credentials reset using a physical Button
- *************************************************************/
-
-/* Fill in information from your Blynk Template here */
-/* Read more: https://bit.ly/BlynkInject */
-//#define BLYNK_TEMPLATE_ID           "TMPxxxxxx"
-//#define BLYNK_TEMPLATE_NAME         "Device"
-#define BLYNK_TEMPLATE_ID "TMPL332wqwwYu"
-#define BLYNK_TEMPLATE_NAME "esp32bmp180"
-#define BLYNK_FIRMWARE_VERSION        "0.1.0"
+#define BLYNK_TEMPLATE_ID "Your Template id"
+#define BLYNK_TEMPLATE_NAME "Your Template name"
+#define BLYNK_FIRMWARE_VERSION        "0.1.0" //enter the version which you have
 
 #define BLYNK_PRINT Serial
 //#define BLYNK_DEBUG
